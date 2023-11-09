fb84d3dc9084d00d8578b4c753a901b864c747c7
indraKurniawan555 <113221828+indraKurniawan555@users.noreply.github.com>
2023-11-09 20:58:36 +0700

    Add files via upload

---
+++ PROJECT5_220441100017_INDRA KURNIAWAN/src/tugasmodul5/KoneksiData.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugasmodul5;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class KoneksiData {
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL      = "jdbc:mysql://localhost/pemvis";
+    private static final String USER        = "root";
+    private static final String PASS        = "";
+    
+    private Connection conn                 = null;
+    
+    public KoneksiData(){
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Koneksi Succes");
+        } catch (Exception e) {
+            System.out.println("Koneksi GAGAL");
+        }
+    }
+    
+    public Connection getConnection(){
+        return conn;
+    }
+    
+}
